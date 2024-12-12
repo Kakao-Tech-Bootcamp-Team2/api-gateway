4028943212db53a22d25f13529c1b7de9a3baf9a
DS0708 <jonum12312@gmail.com>
2024-12-12 21:33:23 +0900

    레이트 리미터 잠깐 꺼둠

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,6 +1,7 @@
+ // Start of Selection
 module.exports = {
   correlationId: require('./correlationId'),
   errorHandler: require('./errorHandler'),
   requestLogger: require('./requestLogger'),
-  rateLimiter: require('./rateLimiter')
+  // rateLimiter: require('./rateLimiter') // 레이트리미터 잠깐 꺼 둠
 };
