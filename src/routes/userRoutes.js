90c09118753d7f0ceae2bdc17a1cc10cc0ac6e96
DS0708 <jonum12312@gmail.com>
2024-12-13 01:18:18 +0900

    profile get 추가

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,6 +3,22 @@ const router = express.Router();
 const httpClient = require('../utils/httpClient');
 const services = require('../config/services');
 
+
+// 프로필 조회
+router.get('/profile', async (req, res, next) => {
+  try {
+    const response = await httpClient.get(
+      `${services.auth.url}/api/v1/users/profile`,
+      {
+        headers: req.headers
+      }
+    );
+    res.json(response.data);
+  } catch (error) {
+    next(error);
+  }
+});
+
 // 프로필 수정 (비밀번호 변경)
 router.put('/profile', async (req, res, next) => {
   try {
